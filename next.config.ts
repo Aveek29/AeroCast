0a24ceb77de471e65f5cf526da1545bc0a188189
Aveek29 <aveekpatel@gmail.com>
2026-06-19 20:46:18 +0530

    refactor: flatten directory structure

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   compress: true,
   productionBrowserSourceMaps: false,
-  allowedDevOrigins: ["192.168.1.35"],
 };
 
 export default nextConfig;
